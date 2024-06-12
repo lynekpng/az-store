b09e6c3ccd69d21a93613d0bb9831dde21d7a9c0
Stacy Druart <stacy@StacyDruartMSI>
2024-06-12 14:48:24 +0200

    maj

--- index.php
+++ index.php
@@ -1,31 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Az Store</title>
-    <link rel="stylesheet" href="../assets/php/index.php">
-    <link rel="stylesheet" href="assets/css/main.css">
+    <!-- Correction du lien de feuille de style mal placé -->
+    <link rel="stylesheet" href="assets/css/main2.css">
     <script src="script.js" defer></script>
 </head>
-
 <body>
+<div class="container">
     <main>
+        <section class="un">
+            <div>
+                <img src="assets/img/shoe_one.png" alt="" class="img_background">
+                <div class="text">
+                    <p>SHOE THE RIGHT <span class="mot_bleu">ONE</span></p>
+                    <button>See our store</button>
+                </div>
+            </div>
+        </section>
         <section>
-        <div>
-            <img src="assets/img/shoe_one.png" alt="" class="img_background">
-            <p>SHOE THE RIGHT <span class="mot_bleu">ONE</span></p>
-            <button>See our store</button>
-        </div>
-        <P>WE PROVIDE YOU THE  <span class="mot_bleu">BEST QUALITE</span></P>
-        <img src="assets/img/shoe_two.png" alt="">
+            <p class="paragraphe">WE PROVIDE YOU THE <span class="mot_bleu">BEST</span> QUALITE</p>
+            <img src="assets/img/shoe_two.png" alt="">
         </section>
     </main>
-    <section class="clients">
-
-    </section>
-
+ 
+</div>
+<footer>
+    <!-- Suppression de la section clients dupliquée -->
+    <div>
+        <img class="img_clients" src="assets/img/image-emily.jpg" alt="">
+        <p><span class="clients_name">Emily from xyx</span><br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum est enim laudantium consectetur corporis quasi saepe, expedita ullam doloremque, quis non! Eveniet alias porro, quisquam voluptatem voluptatum eum atque mollitia.
+        </p>
+    </div>
+    <div>
+        <img class="img_clients" src="assets/img/image-jennie.jpg" alt="">
+        <p><span class="clients_name">Jennie from corporate</span><br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum est enim laudantium consectetur corporis quasi saepe, expedita ullam doloremque, quis non! Eveniet alias porro, quisquam voluptatem voluptatum eum atque mollitia.
+        </p>
+    </div>
+    <div>
+        <img class="img_clients" src="assets/img/image-thomas.jpg" alt="">
+        <p><span class="clients_name">Thomas from Nike</span><br>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci sed nemo error minus ipsum! Dolores animi nulla tempora expedita quisquam vitae. Reprehenderit eos ratione fugit repellat vel fugiat quia sequi.
+        </p>
+    </div>
+</footer>
 </body>
-
 </html>
