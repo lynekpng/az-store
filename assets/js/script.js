4ea057fc331af45d31cf3ab250165f87b8b9b1b5
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-06-13 14:35:17 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,33 @@
 
-const jsonUrl = '../js/products.json';
+const jsonUrl = '../json/products.json';
 
-  fetch(jsonUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('La requête a échoué');
-      }
-      return response.json();
-    })
-    .then(data => {
-      data.forEach(product => {
-  
-        const card = document.createElement('div');
-        card.classList.add('card');
-  
-  
-        card.innerHTML = `
-              <img src="../img/shoe_one.png" alt="${product.product}">
-              <div class="shoesDisplay">
-                <div class="rowShoes">
-                  <h2>${product.product}</h2>
-                  <p>${product.price} €</p>
-                </div>
-                <button onclick="addToCart('${product.id}', '${product.product}', ${product.price}, '${product.image_url}')">Add to cart</button>
-              </div>
-            `;
-        document.querySelector('.articles').appendChild(card);
-      });
-    })
-    .catch(error => {
-      console.error('Erreur lors de la récupération du JSON :', error);
-    });
-  
-    function addToCart(id, product, price, image_url) {
-      let cart = JSON.parse(localStorage.getItem('cart')) || [];
-      const existingProduct = cart.find(item => item.id === id);
-      if (existingProduct) {
-        existingProduct.quantity += 1;
-      } else {
-        cart.push({ id, product, price, image_url, quantity: 1 });
-      }
-      localStorage.setItem('cart', JSON.stringify(cart));
-      updateCart() ;
+fetch(jsonUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('La requête a échoué');
     }
+    return response.json();
+  })
+  .then(data => {
+    data.forEach(product => {
+
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+
+      card.innerHTML = `
+            <img src="../img/shoe_one.png" alt="${product.product}">
+            <div class="shoesDisplay">
+              <div class="rowShoes">
+                <h2>${product.product}</h2>
+                <p>${product.price} €</p>
+              </div>
+              <button>Add to cart</button>
+            </div>
+          `;
+      document.querySelector('.articles').appendChild(card);
+    });
+  })
+  .catch(error => {
+    console.error('Erreur lors de la récupération du JSON :', error);
+  });
