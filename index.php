6a5dd3675a3b22903ec81b3e75fc1e178328c0ee
Stacy Druart <stacy@StacyDruartMSI>
2024-06-14 15:56:30 +0200

    ajout du dark mode

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,11 +6,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="assets/css/main.css">
     <style>
-@import url('https://fonts.googleapis.com/css2?family=Bungee+Outline&family=Jacquard+24&family=Oswald:wght@200..700&family=Pixelify+Sans:wght@400..700&display=swap');
-</style>
+        @import url('https://fonts.googleapis.com/css2?family=Bungee+Outline&family=Jacquard+24&family=Oswald:wght@200..700&family=Pixelify+Sans:wght@400..700&display=swap');
+    </style>
     <title>AZ-Store</title>
     <script src="assets/js/script.js" defer></script>
-    <script src="assets/js/dark_mode.js"></script>
+    <script src="assets/js/dark_mode.js" defer></script> <!-- Ajout de 'defer' pour s'assurer que le script est chargé après le HTML -->
 </head>
 <body>
 
@@ -22,12 +21,13 @@
             <a href="#about">About</a>
             <a href="#product">Product</a>
             <a href="#contact">Contact</a>
-            <button id="toggleBtn">Dark</button>
+            
         </nav>
         <div>
             <a href=""><i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i></a>
             <a href="#login">Login</a>
         </div>
+        <div> <button id="toggleBtn"> dark </button></div>
     </header>
     <hr>
     <main>
@@ -35,6 +35,7 @@
             <section>
                 <h2>SHOE THE <br>RIGHT <span class="mot_bleu"> ONE </span>.</h2>
                 <button>See our store</button>
+                <button>fadd</button>
             </section>
             <div>
                 <p class="nike">Nike</p>
@@ -64,38 +65,4 @@
                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
                     reprehenderit hic. Minima ratione, sunt veniam ea ullam, perferendis 
                     quia eius minus esse animi officiis assumenda nobis, explicabo consectetur 
-                    molestiae praesentium?"
-                </p>
-            </section>
-            <section>
-                <img src="assets/img/image-thomas.jpg" alt="image-thomas">
-                <h4>Thomas from corporate</h4>
-                <p>
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
-                    reprehenderit hic. Minima ratione, sunt veniam ea ullam, perferendis 
-                    quia eius minus esse animi officiis assumenda nobis, explicabo consectetur 
-                    molestiae praesentium?"
-                </p>
-            </section>
-            <section>
-                <img src="assets/img/image-jennie.jpg" alt="image-jennie">
-                <h4>Jennie from Nike</h4>
-                <p>
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
-                    reprehenderit hic. Minima ratione, sunt veniam ea ullam, perferendis 
-                    quia eius minus esse animi officiis assumenda nobis, explicabo consectetur 
-                    molestiae praesentium?"
-                </p>
-            </section>
-        </div>
-    </main>
-    <hr>
-    <footer>
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#product">Product</a>
-        <a href="#contact">Contact</a>
-
-    </footer>
-</body>
-</html>
+                    molestiae prae
